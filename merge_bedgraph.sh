c033aebaa2ad51652ac46fcb09f8ffb945f43b86
Soo Lee <duplexa@gmail.com>
2017-10-10 16:13:19 -0400

    Update merge_bedgraph.sh

--- merge_bedgraph.sh
+++ merge_bedgraph.sh
@@ -1,4 +1,12 @@
+#!/bin/bash
 ## (Optional) Merge your bedgraph files for further analysis. 
+
+# input files, loess smoothed bedgraph files
+loess_bg_files=@_  # array of files
+
+# output file name
+outfile=merge_Loess_norm_RT.txt 
+
 cd path/to/files/ 
 #  Merge the loess smoothed bedgraph files:
-bedtools unionbedg -filler "NA" -i *Loess.bedGraph > merge_Loess_norm_RT.txt 
+bedtools unionbedg -filler "NA" -i $loess_bg_files > $outfile 
