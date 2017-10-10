f92f51a57d12638b4308a89dbdc618cc0c80fbac
Soo Lee <duplexa@gmail.com>
2017-10-10 16:10:49 -0400

    Update calculate_rt.sh

--- calculate_rt.sh
+++ calculate_rt.sh
@@ -1,6 +1,11 @@
-early_file=
-late_file=
-rt_file=
+#!/bin/bash
+
+# input files
+early_file=$1
+late_file=$2
+
+# output file
+rt_file=$3
 
 # Calculating RT
 paste $early_file $late_file | awk '{if($8 != 0 && $4 != 0){print $1,$2,$3,log($4/$8)/log(2)}}' OFS='\t' > $rt_file
