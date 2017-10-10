be801aa4dbce867e011f8a22059ad5bcd6cd687c
Soo Lee <duplexa@gmail.com>
2017-10-10 16:30:14 -0400

    Update merge_rt.sh

--- merge_rt.sh
+++ merge_rt.sh
@@ -1,5 +1,14 @@
-rt_files=  # array of files
-merged_rt_file=
+#!/bin/bash
+## Usage: merge_rt.sh outdir in1.bg in2.bg in3.bg ...
+
+outdir=$1
+shift
+
+INFILESTR=${@}  # input files as a string
+INFILES=(${INFILESTR// / })  # input files as an array
+
+rt_files=$INFILESTR  # array of files, as a space-delimited string
+merged_rt_file=$outdir/merged_rt.bg
 
 # Merging RT files
 bedtools unionbedg -filler "NA" -i $rt_files > $merged_rt_file
